dec946731737fa1f362d15b997a43e21ca7dbf20
mrepol742 <mrepol742@protonmail.com>
2021-07-11 01:13:46 +0800

    Initial Commit

--- search.js
+++ search.js
@@ -30,12 +30,6 @@ opt.setAttribute("value", atob(su[i]))
 suggestions.appendChild(opt)
 }
 }
-/*//TODO
-for (let i = 0 < 8; i++) {
-    let sug = document.createElement("a")
-    sug.setAttribute("href", atob(su[i]))
-    bot.appendChild(sug)
-}*/ 
 
 function a() {
     let t = search.value;
